2d8a01a5777c0e26f698f8d4729d69f040d18f5c
Peter Meehan <meehanp2@tcd.ie>
2017-12-09 13:17:04 +0000

    add boring js solutions for day 9 :nine:

---
+++ 09/js/part1.js
@@ -0,0 +1,75 @@
+const fs = require('fs')
+
+const contents = fs.readFileSync('../input.txt', 'utf8')
+const chars = contents.split('\n').filter(line => line !== '')[0].split('')
+
+let groupScoreTotal = 0
+let currentDepth = 1
+let states = []
+states.push('seeking-open-curly-or-angle')
+
+for (let index in chars) {
+  const currentChar = chars[index]
+
+  switch (states[states.length - 1]) {
+    case 'seeking-open-curly-or-angle':
+      switch (currentChar) {
+        case '{':
+          groupScoreTotal += currentDepth
+          currentDepth++
+          states.pop()
+          states.push('seeking-closing-curly')
+          break
+        case '<':
+          states.pop()
+          states.push('seeking-closing-angle')
+          break
+        default:
+          throw new Error(`only groups or garbage allowed at the top level, found ${currentChar}`)
+      }
+      break
+    case 'seeking-closing-curly':
+      switch (currentChar) {
+        case '{':
+          groupScoreTotal += currentDepth
+          currentDepth++
+          states.push('seeking-closing-curly')
+          break
+        case '<':
+          states.push('seeking-closing-angle')
+          break
+        case ',':
+          states.push('seeking-open-curly-or-angle')
+          break
+        case '}':
+          currentDepth--
+          states.pop()
+          break
+        default:
+          throw new Error(`I'm looking for a closing curly pls`)
+      }
+      break
+    case 'seeking-closing-angle':
+      switch (currentChar) {
+        case '>':
+          states.pop()
+          break
+        case '!':
+          states.push('ignore-next')
+          break
+        default:
+          // ignore garbage
+          break
+      }
+      break
+    case 'ignore-next':
+      states.pop()
+      break
+    default:
+      throw new Error('Unexpected state')
+  }
+}
+
+if (states.length !== 0) throw new Error('Stack not empty after all input consumed')
+
+console.log(groupScoreTotal)
